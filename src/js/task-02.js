ece87ad1d70822b85249e747d60bc8a64c2ab8d6
Oleksandr Chukhlib <Chukhlib.aleksandr@gmail.com>
2022-01-15 17:40:55 +0200

    Finished task №2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ingredientsRef = document.querySelector("ul#ingredients");
+
+const listRef = ingredients.forEach((ingredients) => {
+  const itemRef = document.createElement("li");
+  itemRef.textContent = ingredients;
+  itemRef.classList.add("item");
+  ingredientsRef.append(itemRef);
+});
+
+console.log(ingredientsRef);
