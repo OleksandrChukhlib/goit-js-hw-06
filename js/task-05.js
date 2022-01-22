53027d554a5925822af7299acd5044cea45bcc2f
Oleksandr Chukhlib <Chukhlib.aleksandr@gmail.com>
2022-01-22 16:21:10 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,17 +1,11 @@
 
-const refs = {
-  input: document.querySelector('#name-input'),
-  output: document.querySelector('#name-output'),
-};
+const nameInput = document.querySelector('#name-input');
+const output = document.querySelector("#name-output");
 
-
-
-refs.input.addEventListener('input', onInputChange);
-
-
-
-function onInputChange(event) { 
-  refs.output.textContent = event.currentTarget.value;
-}
-
+nameInput.addEventListener('input', (event) => { 
+    output.textContent = event.currentTarget.value;
+    if (!event.currentTarget.value) { 
+        output.textContent = 'Anonymous'; 
+    }
+});
 
